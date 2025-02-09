eb6a7cab63b1b69490f3caeb17f1563e3d7a18ca
Kilezico <jgrangeirocosta@gmail.com>
2025-02-09 09:59:28 -0300

    OR e SLT, alem das operacoes de shift sem imediato

--- design/alu.sv
+++ design/alu.sv
@@ -17,10 +17,20 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB;
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+            4'b0100:        // SHIFT LEFT
+                    ALUResult = SrcA << SrcB[4:0];
+            4'b0101:        // SHIFT RIGHT (LOGIC)
+                    ALUResult = SrcA >> SrcB[4:0];
+            4'b0111:        // SHIFT RIGHT (ARITHMETIC)
+                    ALUResult = $signed(SrcA) >>> SrcB[4:0];
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1100:        // Less than SIGNED
+                    ALUResult = $signed(SrcA) < $signed(SrcB);
             default:
                     ALUResult = 0;
             endcase
