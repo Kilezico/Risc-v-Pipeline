7f299dd7e54eab175499b7f45f65860232374b64
João Fernando <154644455+joaofgbarros@users.noreply.github.com>
2025-02-23 12:23:40 -0300

    Update BranchUnit.sv

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -21,7 +21,7 @@ module BranchUnit #(
   logic Branch_Sel;
   logic [31:0] PC_Full;
 
-  assign PC_Full = {23'b0, Cur_PC}; // Aqui é feita uma concatenação de vetores para expandir Cur_PC a ter 32 bits (32 - 9 = 23)
+    assign PC_Full = {23'b0, Cur_PC}; // Aqui eh feita uma concatenacao de vetores para expandir Cur_PC a ter 32 bits (32 - 9 = 23)
 
   assign PC_Imm = PC_Full + Imm;
   assign PC_Four = PC_Full + 32'b100;
